eef254b28c7e4d37e8b2650c59368a1e9b8392b2
steeltx <realsteel.gnu@gmail.com>
2024-03-13 22:12:43 -0600

    Configuración de store y slices de counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import reactLogo from './assets/react.svg';
+import { increment } from './store/slices/counter/';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+	// obtener del estado el contador
+	const { counter } = useSelector( (state) => state.counter);
+	const dispatch = useDispatch();
+
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>count is {counter}</h1>
+			<div className="card">
+				<button onClick={() => dispatch(increment() )}>
+					Increment
+				</button>
+			</div>
+		</>
+	)
 }
 
 export default App
